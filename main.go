79620f7bc919d4799c5f23ddfb51625bf3ebfac4
robert-virtual <robertocastill63@gmail.com>
2022-05-23 16:10:11 -0600

    cors

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	ConnectToDB()
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+	}))
 	products := router.Group("/products")
 	{
 		products.GET("", getProduct)
